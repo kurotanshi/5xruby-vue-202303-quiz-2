62d0ca373f4e09a5ba3d19ebb5b44fe6e1b9f7ab
Russell Lin <superbr7789@gmail.com>
2023-03-12 20:55:15 +0800

    successfully made table and search vue components

---
+++ src/views/uBikeTable/components/search.vue
@@ -0,0 +1,21 @@
+<template>
+  <p>
+    站點名稱搜尋:
+    <input
+      type="text"
+      class="border"
+      :value="modelValue"
+      @input="$emit('update:modelValue', $event.target.value)"
+    />
+  </p>
+</template>
+
+<script setup>
+defineProps({
+  modelValue: {
+    type: String,
+  },
+});
+</script>
+
+<style scoped></style>
--- src/views/uBikeTable/uBikeTable.vue
+++ src/views/uBikeTable/uBikeTable.vue
@@ -1,6 +1,9 @@
 <script setup>
-import { ref, computed, watch } from 'vue';
+import { ref, computed, watch } from "vue";
 import "bootstrap/dist/css/bootstrap.css";
+import search from "./components/search.vue";
+import uBikeTable from "./components/uBikeTable.vue";
+import pagination from "./components/pagination.vue";
 
 // 修改這份 YouBike 即時資訊表：
 // 1. 將搜尋的部分拆出來變成子元件 `uBikeTable/components/search.vue`
@@ -16,29 +19,27 @@ import "bootstrap/dist/css/bootstrap.css";
 // snaen：場站名稱(英文)、 aren：地址(英文)、 bemp：空位數量、 act：全站禁用狀態
 
 // 目前的排序選項
-const currentSort = ref('sno');
+const currentSort = ref("sno");
 // 是否為降冪排序
 const isSortDesc = ref(false);
 // 所有站點資料
 const uBikeStops = ref([]);
 // 搜尋文字
-const searchText = ref('');
+const searchText = ref("");
 // 目前頁碼
 const currentPage = ref(1);
 // 一頁幾筆資料
 const COUNT_OF_PAGE = 20;
 // 最多顯示幾頁
 const PAGINATION_MAX = 10;
-fetch('https://tcgbusfs.blob.core.windows.net/dotapp/youbike/v2/youbike_immediate.json')
-  .then(res => res.text())
-  .then(data => {
+fetch(
+  "https://tcgbusfs.blob.core.windows.net/dotapp/youbike/v2/youbike_immediate.json"
+)
+  .then((res) => res.text())
+  .then((data) => {
     uBikeStops.value = JSON.parse(data);
   });
-const timeFormat = (val) => {
-  // 時間格式
-  const pattern = /(\d{4})(\d{2})(\d{2})(\d{2})(\d{2})(\d{2})/;
-  return val.replace(pattern, '$1/$2/$3 $4:$5:$6');
-};
+
 // 監聽搜尋文字，若有變動則將頁碼歸 1
 watch(searchText, () => {
   currentPage.value = 1;
@@ -47,7 +48,7 @@ watch(searchText, () => {
 const filtedUbikeStops = computed(() => {
   return uBikeStops.value.length === 0
     ? []
-    : uBikeStops.value.filter(d => d.sna.includes(searchText.value));
+    : uBikeStops.value.filter((d) => d.sna.includes(searchText.value));
 });
 // 排序後的站點資料
 const sortedUbikeStops = computed(() => {
@@ -84,83 +85,32 @@ const pagerAddAmount = computed(() => {
   return tmp <= 0
     ? 0
     : totalPageCount.value - (PAGINATION_MAX + tmp) < 0
-      ? totalPageCount.value - PAGINATION_MAX
-      : tmp;
+    ? totalPageCount.value - PAGINATION_MAX
+    : tmp;
 });
 // 換頁
-const setPage = page => {
+const setPage = (page) => {
   if (page < 1 || page > totalPageCount.value) {
     return;
   }
   currentPage.value = page;
 };
-// 指定排序
-const setSort = sortType => {
-  if (sortType === currentSort.value) {
-    isSortDesc.value = !isSortDesc.value;
-  } else {
-    currentSort.value = sortType;
-    isSortDesc.value = false;
-  }
-};
-// 關鍵字 Highlight
-const keywordsHighlight = (text, keyword) => {
-  const reg = new RegExp(keyword, 'gi');
-  return text.replace(reg, `<span style="color: red;">${keyword}</span>`);
-};
 </script>
 
 <template>
   <div class="app">
-    <p>
-      站點名稱搜尋: <input type="text" class="border" v-model="searchText">
-    </p>
+    <!-- search -->
+    <search v-model="searchText" />
 
-    <table class="table table-striped">
-      <thead>
-        <tr>
-          <th @click="setSort('sno')">
-            #
-            <span v-show="currentSort === 'sno'">
-              <i class="fa" :class="isSortDesc ? 'fa-sort-desc' : 'fa-sort-asc'" aria-hidden="true"></i>
-            </span>
-          </th>
-          <th>
-            場站名稱
-          </th>
-          <th>
-            場站區域
-          </th>
-          <th @click="setSort('sbi')" class="pointer">
-            目前可用車輛
-            <span v-show="currentSort === 'sbi'">
-              <i class="fa" :class="isSortDesc ? 'fa-sort-desc' : 'fa-sort-asc'" aria-hidden="true"></i>
-            </span>
-          </th>
-          <th @click="setSort('tot')" class="pointer">
-            總停車格
-            <span v-show="currentSort === 'tot'">
-              <i class="fa" :class="isSortDesc ? 'fa-sort-desc' : 'fa-sort-asc'" aria-hidden="true"></i>
-            </span>
-          </th>
-          <th>
-            資料更新時間
-          </th>
-        </tr>
-      </thead>
-      <tbody>
-        <!-- 替換成 slicedUbikeStops -->
-        <tr v-for="s in slicedUbikeStops" :key="s.sno">
-          <td>{{ s.sno }}</td>
-          <!-- <td>{{ s.sna }}</td> -->
-          <td v-html="keywordsHighlight(s.sna, searchText)"></td>
-          <td>{{ s.sarea }}</td>
-          <td>{{ s.sbi }}</td>
-          <td>{{ s.tot }}</td>
-          <td>{{ timeFormat(s.mday) }}</td>
-        </tr>
-      </tbody>
-    </table>
+    <!-- uBikeTable -->
+    <uBikeTable
+      :ubike-stops="slicedUbikeStops"
+      :search-text="searchText"
+      :current-sort="currentSort"
+      :is-sort-desc="isSortDesc"
+      @changeSortType="(type) => (currentSort = type)"
+      @toggleSort="(boolean) => (isSortDesc = boolean)"
+    />
   </div>
 
   <!-- 頁籤 -->
@@ -170,8 +120,13 @@ const keywordsHighlight = (text, keyword) => {
         <a class="page-link" href>Previous</a>
       </li>
 
-      <li v-for="i in pagerEnd" :class="{ active: i + pagerAddAmount === currentPage }" :key="i"
-        @click.prevent="setPage(i + pagerAddAmount)" class="page-item">
+      <li
+        v-for="i in pagerEnd"
+        :class="{ active: i + pagerAddAmount === currentPage }"
+        :key="i"
+        @click.prevent="setPage(i + pagerAddAmount)"
+        class="page-item"
+      >
         <a class="page-link" href>{{ i + pagerAddAmount }}</a>
       </li>
 
@@ -192,13 +147,5 @@ const keywordsHighlight = (text, keyword) => {
 .pagination {
   display: flex;
   justify-content: center;
-}
-@media (max-width: 768px) {
-  .sno {
-    max-width: 50px; word-wrap: break-word;
-  }
-  .table td, .table th {
-    padding: .5rem .25rem;
-  }
 }
 </style>
